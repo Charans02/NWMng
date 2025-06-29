f7573b3464e7e030f77b511e0712edf9e87ddb61
Charans02 <luckycharan2002@gmail.com>
2025-06-29 14:49:19 +0530

    Updated Page3

--- src/sections/service-areas.tsx
+++ src/sections/service-areas.tsx
@@ -9,7 +9,7 @@ const ServiceAreas = () => {
     <section>
       <div className="flex flex-col items-center justify-between bg-[url('/images/world_map.png')] bg-cover bg-center xl:flex-row">
         <div className="flex flex-col items-center xl:items-start order-1 xl:order-2">
-          <div className="bg-lime/10 w-fit rounded-full px-8 py-4">
+          <div className="bg-lime-900/10 w-fit rounded-full px-8 py-4">
             <p className="text-lime-500 font-[family-name:var(--font-lato-sans)] text-[18px] leading-[18px] font-semibold uppercase">
               Service Areas
             </p>
